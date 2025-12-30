ed8f1e763245b12b1b274989778f6663b98314e9
Lillo <lillofriscia@gmail.com>
2025-12-30 16:31:37 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LFPopKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LFPopKit",
+            targets: ["LFPopKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LFPopKit"
+        ),
+
+    ]
+)
